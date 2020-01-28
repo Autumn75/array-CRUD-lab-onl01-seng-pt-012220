701843ad5f8cde61fd9e97c0bc1a3be974a134cd
Autumn Taylor <autfall75@hotmail.com>
2020-01-28 20:05:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,30 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+  array= ["cherries", "peaches","lemons", "apples"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
-
-def add_element_to_start_of_array(array, element)
   
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
+  # arr = ["cherries","pear","apple","peach"]
   
 end
 
